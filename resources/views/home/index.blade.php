d8caff7d584f0b81baad491e6bed5032cead9a6d
Oscar Moreno <oscarlmorenojorge@gmail.com>
2024-02-16 11:43:23 +0000

    Creacion de la vista para el index

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.home')
+@section("title", $viewData["title"])
+
+@section('content')
+<div class="row">
+    <div class="col-md-6 col-lg-4 mb-2">
+        <!--Aqui va el contenido de la pagina que en principio será una muestra de las gráficas-->
+    </div>
+</div>
+@endsection
