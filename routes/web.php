1f7c89f1bfb6cde9089f9287ef3733d6c8f062c5
oscarlmj <oscarlmorenojorge@gmail.com>
2024-02-18 14:39:33 +0000

    Añadida la ruta para la vista del grafico relativo al consumo de agua

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/water', 'App\Http\Controllers\echartController@water')->name("charts.water");
